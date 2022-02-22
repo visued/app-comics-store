3b7bfc2cd5e835a2c6e247c3fde00df719c81128
Victor Sued <visued@gmail.com>
2022-02-21 22:42:37 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   api: 'https://gateway.marvel.com/v1/public/comics',
   privateKey: '2dbd2c28be39ad06488e6793dce5478c70902618',
   publicKey: '620d4301f463f50b77f1224d087be22e',
-  apiStore: 'https://localhost:5001'
+  apiStore: 'http://localhost:5000'
 };
 
 /*
